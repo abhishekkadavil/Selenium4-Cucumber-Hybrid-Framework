a6bff644fe2c9cea4571002511a346af5f28f41d
abhishekkadavil <abhishek.kadavil@gmail.com>
2025-02-11 23:42:25 +0530

    handled error in @Before and @After

--- src/main/java/com/stepdef/Hooks.java
+++ src/main/java/com/stepdef/Hooks.java
@@ -26,43 +26,53 @@ public class Hooks {
     @Before
     public void beforeScenario(Scenario scenario) {
 
-        log.info("beforeScenario {}", scenario.getName());
+        try{
+            log.info("beforeScenario {}", scenario.getName());
 
-        // Initialising the driver
-        testContext.invokeDriver();
-        testContext.getDriver().navigate().to(testContext.getConfigUtil().getBaseURL());
+            // Initialising the driver
+            testContext.invokeDriver();
+            testContext.getDriver().navigate().to(testContext.getConfigUtil().getBaseURL());
 
-        // Create new test for each scenario
-        ExtentTest test = RunnerHelper.extent.createTest(scenario.getName());
-        ReporterFactory.getInstance().setExtentTestList(test);
+            // Create new test for each scenario
+            ExtentTest test = RunnerHelper.extent.createTest(scenario.getName());
+            ReporterFactory.getInstance().setExtentTestList(test);
 
-        // Pass percentage execution control logic
-        if (testContext.getConfigUtil().getPassTestNoExecutionControlFlag() && PassTestNoExecutionControl.shouldSkipTest(testContext.getConfigUtil().getPassTestNoExecutionControlValue())) {
-            log.warn("Skipping further test execution due to failures in the first {} tests.", testContext.getConfigUtil().getPassTestNoExecutionControlValue());
-            throw new SkipException("Skipping test execution as first "+ testContext.getConfigUtil().getPassTestNoExecutionControlValue() +" tests failed.");
+            // Pass percentage execution control logic
+            if (testContext.getConfigUtil().getPassTestNoExecutionControlFlag() && PassTestNoExecutionControl.shouldSkipTest(testContext.getConfigUtil().getPassTestNoExecutionControlValue())) {
+                log.warn("Skipping further test execution due to failures in the first {} tests.", testContext.getConfigUtil().getPassTestNoExecutionControlValue());
+                throw new SkipException("Skipping test execution as first "+ testContext.getConfigUtil().getPassTestNoExecutionControlValue() +" tests failed.");
+            }
+        }
+        catch (Exception e){
+            log.error("Setup failed in @Before hook: ", e);
         }
     }
 
     @After
     public void afterScenario(Scenario scenario) {
 
-        log.info("afterScenario {}", scenario.getName());
+        try{
+            log.info("afterScenario {}", scenario.getName());
 
-        //Passed step adding screenshot
-        if (scenario.getStatus().toString().equalsIgnoreCase("PASSED")) {
-            ReporterFactory.getInstance().getExtentTest().pass(MediaEntityBuilder.createScreenCaptureFromBase64String(interactionHelper.takeScreenShotOfWebPage()).build());
-        } else if (scenario.getStatus().toString().equalsIgnoreCase("FAILED")) {
-            ReporterFactory.getInstance().getExtentTest().fail(MediaEntityBuilder.createScreenCaptureFromBase64String(interactionHelper.takeScreenShotOfWebPage()).build());
+            //Passed step adding screenshot
+            if (scenario.getStatus().toString().equalsIgnoreCase("PASSED")) {
+                ReporterFactory.getInstance().getExtentTest().pass(MediaEntityBuilder.createScreenCaptureFromBase64String(interactionHelper.takeScreenShotOfWebPage()).build());
+            } else if (scenario.getStatus().toString().equalsIgnoreCase("FAILED")) {
+                ReporterFactory.getInstance().getExtentTest().fail(MediaEntityBuilder.createScreenCaptureFromBase64String(interactionHelper.takeScreenShotOfWebPage()).build());
+            }
+
+            // Pass percentage execution control logic
+            if (testContext.getConfigUtil().getPassTestNoExecutionControlFlag() && scenario.isFailed()) {
+                PassTestNoExecutionControl.incrementFailureCount();
+            }
         }
-
-        //Close browser
-        testContext.quitDriver();
-
-        // Pass percentage execution control logic
-        if (testContext.getConfigUtil().getPassTestNoExecutionControlFlag() && scenario.isFailed()) {
-            PassTestNoExecutionControl.incrementFailureCount();
+        catch (Exception e){
+            log.error("Setup failed in @After hook: ", e);
         }
-
+        finally {
+            //Close browser
+            testContext.quitDriver();
+        }
     }
 
 
